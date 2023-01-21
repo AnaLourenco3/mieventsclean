2f68788cdcffd91197b43941d19883d6034227a9
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2023-01-21 13:59:10 +0100

    backend

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,6 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  "https://mieventsbackend-analourenco3.onrender.com" ||
+  "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
+
+// or API_URL instead of the link if .env is used
