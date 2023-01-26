45980a3c697ed71e2b546302b7768f131e7a4dee
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2023-01-26 15:53:03 +0000

    fix issue

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,5 +2,3 @@ export const apiUrl =
   "https://mieventsbackend-analourenco3.onrender.com" ||
   "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
-
-// or API_URL instead of the link if .env is used
